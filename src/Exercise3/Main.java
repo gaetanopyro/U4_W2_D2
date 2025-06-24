966c71dda2625e03deab6fd04382036edff3001e
gaetanopyro <gaetano.miscio@gmail.com>
2025-06-24 17:04:49 +0200

    finish

--- src/Exercise3/Main.java
+++ src/Exercise3/Main.java
@@ -11,6 +11,10 @@ public class Main {
         phoneBook.put("Luca", "329898987");
         phoneBook.put("Maria", "3381213145");
         phoneBook.put("Mario", "3342324256");
+        phoneBook.put("Gianna", "33345464789");
+        phoneBook.put("Marta", "3313334356");
+        phoneBook.put("Jack", "3457879987");
+
         System.out.println(phoneBook);
         System.out.println("eliminazione nome");
         phoneBook.remove("Luca");
@@ -23,5 +27,15 @@ public class Main {
             System.out.println("name: " + name);
         }
 
+        for (String number : phoneBook.values()) {
+            System.out.println("number: " + number);
+        }
+        /*
+        Collection<String> number = phoneBook.values();
+        System.out.println(number);
+        
+         */
+
+        System.out.println(phoneBook);
     }
 }
